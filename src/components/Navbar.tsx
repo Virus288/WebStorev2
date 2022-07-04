3f02ccb14c202c6652ef1e4a3b893b66649e0831
Jakub <kiszczyckuba20@wp.pl>
2022-07-04 22:05:04 +0200

    Finished basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,40 @@
 import { toggleNav } from '../utils';
+import { useEffect } from 'react';
 
 export default function Navbar(): JSX.Element {
+  const selectButton = (): void => {
+    document.querySelectorAll('button').forEach((bt) => {
+      bt.addEventListener('click', (e) => {
+        e.preventDefault();
+        const activeButton = document.querySelector('.activeButton');
+        const target = e.target as HTMLElement;
+        if (activeButton) {
+          activeButton.classList.toggle('activeButton');
+        }
+        target.classList.toggle('activeButton');
+      });
+    });
+  };
+
+  useEffect(() => {
+    selectButton();
+  }, []);
+
   return (
     <>
       <div id="navbar">
         <div>
-          <button>Button1</button>
-          <button>Button2</button>
-          <button>Button3</button>
+          <h1>Capri</h1>
         </div>
         <div>
-          <button>Button4</button>
+          <button>Pościele</button>
+          <button>Prześcieradła</button>
+          <button>Poduszki</button>
+          <button>Koce</button>
+          <button>Ręczniki</button>
+        </div>
+        <div>
+          <button>Koszyk</button>
           <button>
             <i className="icon-adult navIcon" />
           </button>
